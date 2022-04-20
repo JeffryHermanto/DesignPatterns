24981fd69b4c7eb5ff2095b626e5f1d438c9a543
Jeffry Hermanto <jeffryhermanto@gmail.com>
2022-04-20 09:52:33 +0700

    Add Memento Pattern

---
+++ src/com/jeffryhermanto/Main.java
@@ -0,0 +1,23 @@
+package com.jeffryhermanto;
+
+import com.jeffryhermanto.memento.Editor;
+import com.jeffryhermanto.memento.History;
+
+public class Main {
+    public static void main(String[] args) {
+        var editor = new Editor();
+        var history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+    }
+}
+
