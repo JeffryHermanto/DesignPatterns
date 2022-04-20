18dfbfac7fc64407285b17c71b623f446bf407bd
Jeffry Hermanto <jeffryhermanto@gmail.com>
2022-04-20 14:33:45 +0700

    Add Iterator Pattern

--- src/com/jeffryhermanto/Main.java
+++ src/com/jeffryhermanto/Main.java
@@ -1,14 +1,23 @@
 package com.jeffryhermanto;
 
+import com.jeffryhermanto.iterator.BrowseHistory;
+import com.jeffryhermanto.iterator.Iterator;
 import com.jeffryhermanto.state.Canvas;
 import com.jeffryhermanto.state.SelectionTool;
 
 public class Main {
     public static void main(String[] args) {
-        var canvas = new Canvas();
-        canvas.setCurrentTool(new SelectionTool());
-        canvas.mouseDown();
-        canvas.mouseUp();
+        var history = new BrowseHistory();
+        history.push("https://google.com");
+        history.push("https://instagram.com");
+        history.push("https://twitter.com");
+
+        var iterator = history.createIterator();
+        while (iterator.hasNext()) {
+            var url = iterator.current();
+            System.out.println(url);
+            iterator.next();
+        }
     }
 }
 
